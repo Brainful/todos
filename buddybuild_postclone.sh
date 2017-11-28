2092a0109b481aa20b95c519c364fd9dfd53b224
Christopher deCharms <decharms@gmail.com>
2017-11-28 14:10:16 -0700

    Added buddybuild script file.

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,4 +2,4 @@ curl https://install.meteor.com/ | sh
 meteor npm install
 echo ** npm install completed **
 meteor add-platform ios
-meteor build ios --server app.fanato.net
+meteor build ios --server https://app.fanato.net:433
